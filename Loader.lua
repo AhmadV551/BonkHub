b6176bb757f9a3d5263fd07f99ba841a73be23fe
AhmadV551 <selrach.zone@gmail.com>
2025-10-25 11:47:53 +0800

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,7 +1,1 @@
-loadstring(game:HttpGet("https://pastebin.com/raw/LKjHv2vG", true))()
 
-loadstring(game:HttpGet("https://gist.githubusercontent.com/AhmadV551/5dd1c1b2bad46ac0a48ce7714a401f1b/raw/250f15dca411076f94cbf26e85e45a4b15b29ce1/Bonk%2520Hub.lua", true))()
-
-delay(50, function()
-    loadstring(game:HttpGet("https://bonkhub.online/loader.lua", true))()
-end)
